22edd7aef2f6882ac72dd5b866a2016f9e702081
VictorTadashi <victortadashisaitobarra@gmail.com>
2025-05-05 19:53:38 -0300

    Site 90% completo

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ AOS.init({
     duration: 1000, // Duração da animação (ms)
     once: true      // Se quiser que a animação aconteça só uma vez
   });
+
+  function baixarArquivo() {
+    const link = document.createElement('a');
+    link.href = './arquivos/CV.pdf'; 
+    link.download = 'Curriculo_Victor_Tadashi.pdf'; 
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
